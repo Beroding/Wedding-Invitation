458e432c9040c4ea8a0f2e0baa6e1397405fb13a
Codings01 <bernardlii4225@gmail.com>
2024-01-13 12:26:11 +0700

    first commit

---
+++ countdown.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var countDownDate = new Date("Feb 2, 2024 12:00:00").getTime();
+
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("countdown").innerHTML =
+            days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "Happy Wedding !";
+        }
+    }, 4000);
+});
