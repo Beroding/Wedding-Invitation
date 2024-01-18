08e4cef683e027d1e8dabb959c3336080f293202
Codings01 <bernardlii4225@gmail.com>
2024-01-18 16:30:42 +0700

    countdown

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var countDownDate = new Date("Feb 2, 2024 12:00:00").getTime();
+    var countDownDate = new Date("jan 31, 2024 12:00:00").getTime();
 
     var x = setInterval(function () {
         var now = new Date().getTime();
@@ -12,11 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         document.getElementById("countdown").innerHTML =
-            days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+            days + "Day " + hours + "Hour " + minutes + "Minute " + seconds + "Second ";
 
         if (distance < 0) {
             clearInterval(x);
             document.getElementById("countdown").innerHTML = "Happy Wedding !";
         }
-    }, 4000);
+    }, 
+    1000);
 });
